5e17a26e2d40ebc0da54d8337f25ab42d0b21940
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-08-18 07:46:17 -0600

    setup switchboard environment

--- tests/vrf-client.ts
+++ tests/vrf-client.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { VrfClient } from "../target/types/vrf_client";
+import { SwitchboardTestContext } from "@switchboard-xyz/sbv2-utils";
 import * as sbv2 from "@switchboard-xyz/switchboard-v2";
 
 describe("vrf-client", () => {
@@ -11,8 +12,27 @@ describe("vrf-client", () => {
   const provider = program.provider as anchor.AnchorProvider;
   const payer = (provider.wallet as sbv2.AnchorWallet).payer;
 
+  let switchboard: SwitchboardTestContext;
+
+  before(async () => {
+    switchboard = await SwitchboardTestContext.loadFromEnv(
+      program.provider as anchor.AnchorProvider,
+      undefined,
+      5_000_000 // .005 wSOL
+    );
+    const queueData = await switchboard.queue.loadData();
+    console.log(`oracleQueue: ${switchboard.queue.publicKey}`);
+    console.log(
+      `unpermissionedVrfEnabled: ${queueData.unpermissionedVrfEnabled}`
+    );
+    console.log(`# of oracles heartbeating: ${queueData.queue.length}`);
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      `\u2714 Switchboard localnet environment loaded successfully\n`
+    );
+  });
+
   it("init_client", async () => {
-    // Add your test here.
     const tx = await program.methods.initClient({}).rpc();
     console.log("init_client transaction signature", tx);
   });
